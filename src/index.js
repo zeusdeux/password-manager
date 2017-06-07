f0c3c307d524d041611b25aab9939822f9768b6e
Mudit Ameta <zeusdeux@gmail.com>
2017-06-07 19:09:27 +0200

    Setup basics

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const { join } = require('path')
+const { format } = require('url')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 })
+
+  win.loadURL(format({
+    pathname: join(__dirname, '../views/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (!win) {
+    createWindow()
+  }
+})
