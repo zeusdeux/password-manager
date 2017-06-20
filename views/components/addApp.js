e62c5f46a629ef292be63f13212774e7ffce0b3c
Mudit Ameta <zeusdeux@gmail.com>
2017-06-21 01:29:34 +0200

    Replace .bind with @bind decorator

--- views/components/addApp.js
+++ views/components/addApp.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact'
+import { bind } from './util'
 
 export default class extends Component {
   constructor() {
@@ -10,6 +11,7 @@ export default class extends Component {
       [key]: e.target.value
     })
   }
+  @bind
   toggleShow() {
     this.setState({
       show: !this.state.show
@@ -23,6 +25,7 @@ export default class extends Component {
       show: false
     })
   }
+  @bind
   handleSubmit(e) {
     e.preventDefault()
 
@@ -33,14 +36,14 @@ export default class extends Component {
     return (
       <section>
         <div style={{display: show ? 'block' : 'none'}}>
-          <form onSubmit={this.handleSubmit.bind(this)}>
+          <form onSubmit={this.handleSubmit}>
             <input name='appName' placeholder='Enter app name' value={appName} onInput={this.linkState('appName')} />
             <input name='username' placeholder='Enter app username' value={username} onInput={this.linkState('username')} />
             <input name='password' placeholder='Enter app password' value={password} onInput={this.linkState('password')} />
             <button type='submit'>Save</button>
           </form>
         </div>
-        <button onClick={this.toggleShow.bind(this)}>Add new app</button>
+        <button onClick={this.toggleShow}>Add new app</button>
       </section>
     )
   }
